bd5c5826aadefffcfc1e82cc6b885b195caf6c8f
ITesserakt <potryas85@mail.ru>
2026-03-06 15:49:24 +0300

    Removed all conflicting with stylix options from mangohud config

--- modules/homeModules/mangohud.nix
+++ modules/homeModules/mangohud.nix
@@ -6,10 +6,7 @@
         legacy_layout = false;
 
         round_corners = 0;
-        background_color = "000000";
 
-        font_size = 24;
-        text_color = "FFFFFF";
         position = "top-left";
         toggle_hud = "Shift_R+F1";
         hud_compact = true;
@@ -19,13 +16,11 @@
         gpu_stats = true;
         gpu_load_change = true;
         gpu_load_value = [ 50 90 ];
-        gpu_load_color = [ "FFFFFF" "FFAA7F" "CC0000" ];
         throttling_status = true;
         gpu_core_clock = true;
         gpu_mem_clock = true;
         gpu_temp = true;
         gpu_power = true;
-        gpu_color = "2E9762";
         
         cpu_text = "CPU";
         cpu_stats = true;
@@ -34,25 +29,19 @@
         cpu_mhz = true;
         cpu_temp = true;
         cpu_power = true;
-        cpu_color = "2E97CB";
 
         vram = true;
-        vram_color = "AD64C1";
 
         ram = true;
-        ram_color = "C26693";
 
         fps = true;
         fps_metrics = [ "avg" "0.01" ];
 
         engine_version = true;
-        engine_color = "EB5B5B";
 
         wine = true;
-        wine_color = "EB5B5B";
 
         frame_timing = true;
-        frametime_color = "FFFFFF";
 
         throttling_status_graph = true;
         fps_limit_method = "late";
