5597ff42869615dc98aa9fe59a873b052c504566
ITesserakt <potryas85@mail.ru>
2026-03-10 16:34:32 +0300

    Enable cuda & hyprland substituters

--- modules/nixosModules/nix.nix
+++ modules/nixosModules/nix.nix
@@ -12,15 +12,15 @@
       trusted-users = [ "@wheel" ];
       substituters = [
         "https://hyprland.cachix.org"
-        # "https://cache.nixos-cuda.org"
+        "https://cache.nixos-cuda.org"
         # "https://cosmic.cachix.org/"
       ];
       trusted-substituters = [
         "hyprland.cachix.org"
       ];
       trusted-public-keys = [
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
-        # "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         # "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
       ];
       auto-optimise-store = true;
