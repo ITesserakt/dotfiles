7582cf831aceb282dc9de7ccc42c79b593f29703
ITesserakt <potryas85@mail.ru>
2026-04-20 02:02:32 +0300

    Fixed keyd binds

--- modules/nixosConfigurations/mac-air/default.nix
+++ modules/nixosConfigurations/mac-air/default.nix
@@ -18,32 +18,42 @@
     ];
   };
 
-  flake.nixosModules.mac-air = {
-    imports = [
-      ./_hardware-configuration.nix
-    ];
+  flake.nixosModules.mac-air =
+    { config, lib, ... }:
+    {
+      imports = [
+        ./_hardware-configuration.nix
+      ];
 
-    system.stateVersion = "25.05";
+      system.stateVersion = "25.05";
 
-    boot.loader.systemd-boot.enable = true;
-    boot.loader.grub.enable = lib.mkForce false;
-    boot.loader.efi.canTouchEfiVariables = lib.mkForce false;
-    boot.kernelParams = [
-      "appledrm.show_notch=1"
-    ];
+      boot.loader.systemd-boot.enable = true;
+      boot.loader.grub.enable = lib.mkForce false;
+      boot.loader.efi.canTouchEfiVariables = lib.mkForce false;
+      boot.kernelParams = [
+        "appledrm.show_notch=1"
+      ];
 
-    hardware.asahi = {
-      peripheralFirmwareDirectory = ./firmware;
-      extractPeripheralFirmware = false;
+      boot.kernelPackages =
+        let
+          pkgs' = config.hardware.asahi.pkgs;
+        in
+        lib.mkForce (pkgs'.linux-asahi.override {
+          _kernelPatches = config.boot.kernelPatches;
+        });
+
+      hardware.asahi = {
+        peripheralFirmwareDirectory = ./firmware;
+        extractPeripheralFirmware = false;
+      };
+
+      zramSwap = {
+        enable = true;
+        memoryPercent = 100;
+        algorithm = "zstd";
+      };
+
+      services.udev.extraRules = ''KERNEL=="macsmc-battery", SUBSYSTEM=="power_supply", ATTR{charge_control_end_threshold}="80", ATTR{charge_control_start_threshold}="70"'';
+      services.tuned.enable = true;
     };
-
-    zramSwap = {
-      enable = true;
-      memoryPercent = 100;
-      algorithm = "zstd";
-    };
-
-    services.udev.extraRules = ''KERNEL=="macsmc-battery", SUBSYSTEM=="power_supply", ATTR{charge_control_end_threshold}="80", ATTR{charge_control_start_threshold}="70"'';
-    services.tuned.enable = true;
-  };
 }
