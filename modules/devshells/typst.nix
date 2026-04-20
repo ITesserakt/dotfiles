97f5bdb19dc2ad74dab900c1e30a2b219b04aae8
ITesserakt <potryas85@mail.ru>
2026-04-20 18:36:47 +0300

    Fixed sha256

--- modules/devshells/typst.nix
+++ modules/devshells/typst.nix
@@ -38,7 +38,7 @@
             owner = "ITesserakt";
             repo = "typst-bmstu-presentation";
             rev = "master";
-            sha256 = "sha256-/KnsAu9a7st28qxyT977ddKmviQm7j6HeFwgfr4PJfQ=";
+            sha256 = "sha256-X/PygGN+/CzfNp1n/uuf+SDP02sNN3EA9tphiQCIjL4=";
           };
 
           unpublishedTypstPackages = mkTypstPackagesDrv "unpublished-typst-packages" [
