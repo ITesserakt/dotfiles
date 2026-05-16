3f22659f5e2fceea15c73d116f016bcd43f300b1
ITesserakt <potryas85@mail.ru>
2026-05-16 21:32:32 +0300

    Added typst bmstu thesis template
    Fixed sha256 hash

--- modules/devshells/typst.nix
+++ modules/devshells/typst.nix
@@ -41,6 +41,13 @@
             sha256 = "sha256-/KnsAu9a7st28qxyT977ddKmviQm7j6HeFwgfr4PJfQ=";
           };
 
+          typst-bmstu-thesis = pkgs.fetchFromGitHub {
+            owner = "ITesserakt";
+            repo = "typst-bmstu-thesis";
+            rev = "master";
+            sha256 = "sha256-m6mOIfVghLrenlsvMeXCfdPXdCvJJHyzWN2bDYtdHKg=";
+          };
+
           mkTypstPackage = name: input: {
             inherit name input;
             namespace = "local";
@@ -50,6 +57,7 @@
           unpublishedTypstPackages = mkTypstPackagesDrv "unpublished-typst-packages" [
             (mkTypstPackage "bmstu-report" typst-bmstu-report)
             (mkTypstPackage "bmstu-presentation" typst-bmstu-presentation)
+            (mkTypstPackage "bmstu-thesis" typst-bmstu-thesis)
           ];
         in
         {
