d2e54c6579e45373af993f683e2041056e189c27
ITesserakt <potryas85@mail.ru>
2026-05-21 19:52:56 +0300

    Use fairydust branch for linux kernel

--- modules/nixosConfigurations/mac-air/default.nix
+++ modules/nixosConfigurations/mac-air/default.nix
@@ -21,7 +21,12 @@
   };
 
   flake.nixosModules.mac-air =
-    { config, lib, ... }:
+    {
+      pkgs,
+      config,
+      lib,
+      ...
+    }:
     {
       imports = [
         ./_hardware-configuration.nix
@@ -37,13 +42,18 @@
       ];
       # boot.supportedFilesystems = [ "apfs" ];
 
-      boot.kernelPackages =
-        let
-          pkgs' = config.hardware.asahi.pkgs;
-        in
-        lib.mkForce (pkgs'.linux-asahi.override {
-          _kernelPatches = config.boot.kernelPatches;
-        });
+      boot.kernelPackages = lib.mkForce (
+        config.hardware.asahi.pkgs.linuxPackagesFor (
+          config.hardware.asahi.pkgs.linux-asahi.kernel.overrideAttrs {
+            src = pkgs.fetchFromGitHub {
+              owner = "AsahiLinux";
+              repo = "linux";
+              rev = "f9f31e394acadb47e564a867a3538f6a87db956e";
+              sha256 = "sha256-vT9uGCgi0uKssJ78bctBh8NNR2GnOIPICKtdU1+GQYE=";
+            };
+          }
+        )
+      );
 
       hardware.asahi = {
         peripheralFirmwareDirectory = ./firmware;
