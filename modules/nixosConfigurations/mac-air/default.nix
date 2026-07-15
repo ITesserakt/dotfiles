202cb360bf4efb083bf1c3737cedca4aae15656c
ITesserakt <potryas85@mail.ru>
2026-07-15 03:11:57 +0300

    Enable apple firmware extraction

--- modules/nixosConfigurations/mac-air/default.nix
+++ modules/nixosConfigurations/mac-air/default.nix
@@ -1,6 +1,5 @@
 {
   self,
-  inputs,
   lib,
   ...
 }:
@@ -50,27 +49,24 @@
       ];
 
       boot.supportedFilesystems = [ "apfs" ];
-      # boot.binfmt.emulatedSystems = [ "x86_64-linux" ];
-      # nix.settings.extra-platforms = [ "x86_64-linux" ];
-      # box64-binfmt.enable = false;
 
-      # specialisation.fairydust.configuration.boot.kernelPackages = lib.mkForce (
-      #   config.hardware.asahi.pkgs.linuxPackagesFor (
-      #     config.hardware.asahi.pkgs.linux-asahi.kernel.overrideAttrs {
-      #       src = pkgs.fetchFromGitHub {
-      #         owner = "AsahiLinux";
-      #         repo = "linux";
-      #         rev = "f9f31e394acadb47e564a867a3538f6a87db956e";
-      #         sha256 = "sha256-vT9uGCgi0uKssJ78bctBh8NNR2GnOIPICKtdU1+GQYE=";
-      #       };
-      #       version = "7.0.8";
-      #     }
-      #   )
-      # );
+      specialisation.fairydust.configuration.boot.kernelPackages = lib.mkForce (
+        config.hardware.asahi.pkgs.linuxPackagesFor (
+          config.hardware.asahi.pkgs.linux-asahi.kernel.overrideAttrs {
+            src = pkgs.fetchFromGitHub {
+              owner = "AsahiLinux";
+              repo = "linux";
+              rev = "c83992242bc1e38bfc861a91696534479a2dbdf4";
+              sha256 = "sha256-sGcgrrf/rpb8u9dvwiTFdNjp18UyuRhW94biH1WMO5I=";
+            };
+            version = "7.0.13";
+          }
+        )
+      );
 
       hardware.asahi = {
         peripheralFirmwareDirectory = ./firmware;
-        extractPeripheralFirmware = false;
+        extractPeripheralFirmware = true;
       };
       hardware.sensor.iio.enable = true;
 
