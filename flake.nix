0fe2d4ec834b2b159fb61796fcab10fc24a463ce
ITesserakt <potryas85@mail.ru>
2026-07-25 02:30:20 +0300

    WIP: added gaze module

--- flake.nix
+++ flake.nix
@@ -17,6 +17,10 @@
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    gaze = {
+      url = "github:GunduLabs/gaze/nixos";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
